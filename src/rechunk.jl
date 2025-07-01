e7fa62df7e3f1d5746f758fc1e2fdf2d87fc3252
Felix Cremer <felix.cremer@bgc-jena.mpg.de>
2025-07-01 15:48:04 +0200

    Rename RechunkedDiskArray to MockChunkedDiskArray and rechunk to mockchunk

--- src/rechunk.jl
+++ src/rechunk.jl
@@ -1,21 +1,21 @@
 """
-    RechunkedDiskArray <: AbstractDiskArray
+    MockChunkedDiskArray <: AbstractDiskArray
 
-    RechunkedDiskArray(parent::AbstractArray, chunks::GridChunks)
+    MockChunkedDiskArray(parent::AbstractArray, chunks::GridChunks)
 
-A disk array that forces a specific chunk pattern, 
+A disk array that pretends a specific chunk pattern, 
 regardless of the true chunk pattern of the parent array.
 
 This is useful in `zip` and other operations that can iterate
 over multiple arrays with different patterns.
 """
-struct RechunkedDiskArray{T,N,A<:AbstractArray{T,N},C<:GridChunks} <: AbstractDiskArray{T,N}
+struct MockChunkedDiskArray{T,N,A<:AbstractArray{T,N},C<:GridChunks} <: AbstractDiskArray{T,N}
     parent::A
     chunks::C
 end
 
 """
-  rechunk(data::AbstractArray,chunks)
+  mockchunks(data::AbstractArray,chunks)
 
 Change the chunks of the underlying DiskArray. Note that this will not change the chunking of the underlying data itself, it will just make the data
 "look" like it had a different chunking. If you need a persistent on-disk representation of this chunking, save the resulting array. 
@@ -26,23 +26,23 @@ The chunks argument can take one of the following forms:
 
 •  a tuple specifying the chunk size along each dimension
 """
-rechunk(data::AbstractDiskArray, chunks::GridChunks) = RechunkedDiskArray(data, chunks)
+mockchunks(data::AbstractDiskArray, chunks::GridChunks) = MockChunkedDiskArray(data, chunks)
 
-Base.parent(A::RechunkedDiskArray) = A.parent
-Base.size(A::RechunkedDiskArray) = size(parent(A))
+Base.parent(A::MockChunkedDiskArray) = A.parent
+Base.size(A::MockChunkedDiskArray) = size(parent(A))
 
 # DiskArrays interface
 
-haschunks(::RechunkedDiskArray) = Chunked()
-eachchunk(A::RechunkedDiskArray) = A.chunks
+haschunks(::MockChunkedDiskArray) = Chunked()
+eachchunk(A::MockChunkedDiskArray) = A.chunks
 
 # These could be more efficient with memory in some cases, but this is simple
-readblock!(A::RechunkedDiskArray, data, I...) = _readblock_rechunked(A, data, I...)
-readblock!(A::RechunkedDiskArray, data, I::AbstractVector...) =
-    _readblock_rechunked(A, data, I...)
-writeblock!(A::RechunkedDiskArray, data, I...) = writeblock!(parent(A), data, I...)
+readblock!(A::MockChunkedDiskArray, data, I...) = _readblock_mockchunked(A, data, I...)
+readblock!(A::MockChunkedDiskArray, data, I::AbstractVector...) =
+    _readblock_mockchunked(A, data, I...)
+writeblock!(A::MockChunkedDiskArray, data, I...) = writeblock!(parent(A), data, I...)
 
-function _readblock_rechunked(A, data, I...)
+function _readblock_mockchunked(A, data, I...)
     if haschunks(parent(A)) isa Chunked
         readblock!(parent(A), data, I...)
     else
